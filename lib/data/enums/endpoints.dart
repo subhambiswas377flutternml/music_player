03264bd9dc4337ed1ce7bdf3a3c7814a98b72a2c
SubhamB <subhambiswas129@gmail.com>
2024-08-31 02:50:52 +0530

    Final commit

--- lib/data/enums/endpoints.dart
+++ lib/data/enums/endpoints.dart
@@ -6,8 +6,7 @@ extension EndpointsExtension on EndPoints{
   String val(){
     switch(this){
       case EndPoints.getAudio:
-        // return "https://codeskulptor-demos.commondatastorage.googleapis.com/descent/background%20music.mp3";
-        return "https://drive.usercontent.google.com/u/0/uc?id=1_9kF7atacEEzikCFTbfOnQTnVgLK93dy&export=download";
+        return "https://codeskulptor-demos.commondatastorage.googleapis.com/descent/background%20music.mp3";
     }
   }
 }
